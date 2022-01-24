ce3448d251d704dcc26ef7df6f2151a50f696ffc
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2022-01-23 22:07:50 -0300

    feat(sprite): add sprites for blue map

--- src/game.js
+++ src/game.js
@@ -41,6 +41,13 @@ loadSprite('pipe-top-right', 'hj2GK4n.png')
 loadSprite('pipe-bottom-left', 'c1cYSbt.png')
 loadSprite('pipe-bottom-right', 'nqQ79eI.png')
 
+// blue map
+loadSprite('blue-block', 'fVscIbn.png')
+loadSprite('blue-brick', '3e5YRQd.png')
+loadSprite('blue-steel', 'gqVoI2b.png')
+loadSprite('blue-evil-shroom', 'SvV4ueD.png')
+loadSprite('blue-surprise', 'RMqCc1G.png')
+
 scene('game', ({ level, score })=> {
   layers(['bg','obj','ui'], 'obj')
 
