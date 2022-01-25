17a5a1dbd5675e682363be656ff638f6a993e727
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2022-01-24 22:20:50 -0300

    feat(elements): add blue blocks elements to level config

--- src/game.js
+++ src/game.js
@@ -92,6 +92,11 @@ scene('game', ({ level, score })=> {
     '+': [sprite('pipe-top-right'), solid(), scale(0.5), 'pipe'],
     '^': [sprite('goomba'), 'enemy'],
     '#': [sprite('mushroom'), solid(), 'mushroom', body()],
+    '!': [sprite('blue-block'), solid(), scale(0.5)],
+    '£': [sprite('blue-brick'), solid(), scale(0.5)],
+    'z': [sprite('blue-evil-shroom'), solid(), scale(0.5), 'dangerous'],
+    '@': [sprite('blue-surprise'), solid(), scale(0.5), 'coin-surprise'],
+    'x': [sprite('blue-steel'), solid(), scale(0.5)],
   }
 
   const gameLevel = addLevel(maps[level], levelConfig)
