62973dacd56b104abd5c2b7e01e2290847540220
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2022-01-27 23:43:33 -0300

    fix(enemies): fix blue boomba props

--- src/game.js
+++ src/game.js
@@ -45,7 +45,7 @@ loadSprite('pipe-bottom-right', 'nqQ79eI.png')
 loadSprite('blue-block', 'fVscIbn.png')
 loadSprite('blue-brick', '3e5YRQd.png')
 loadSprite('blue-steel', 'gqVoI2b.png')
-loadSprite('blue-evil-shroom', 'SvV4ueD.png')
+loadSprite('blue-goomba', 'SvV4ueD.png')
 loadSprite('blue-surprise', 'RMqCc1G.png')
 
 scene('game', ({ level, score })=> {
@@ -94,7 +94,7 @@ scene('game', ({ level, score })=> {
     '#': [sprite('mushroom'), solid(), 'mushroom', body()],
     '!': [sprite('blue-block'), solid(), scale(0.5)],
     '&': [sprite('blue-brick'), solid(), scale(0.5)],
-    'z': [sprite('blue-evil-shroom'), solid(), scale(0.5), 'dangerous'],
+    'z': [sprite('blue-goomba'), solid(), scale(0.5), 'enemy'],
     '@': [sprite('blue-surprise'), solid(), scale(0.5), 'coin-surprise'],
     'x': [sprite('blue-steel'), solid(), scale(0.5)],
   }
